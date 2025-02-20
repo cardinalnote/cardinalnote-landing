c54c37c40c1c26ced5f859b53e7512eb78798137
NicholasKoldys <nicholas.koldys@gmail.com>
2025-02-21 01:35:58 +0700

    add dark mode store to hold context;

---
+++ src/lib/utils.ts
@@ -0,0 +1,39 @@
+export function checkForMount( type: "class" | "id" | "tag", idClass: string | undefined ): Promise<Element> {
+  if( idClass == undefined ) idClass = "html";
+
+  return new Promise( (resolve, reject) => {
+    let count = 6;
+    const isMountedInt = setInterval( () => {
+
+      if( count == 0 ) {
+        clearInterval( isMountedInt );
+        reject();
+      }
+      count--;
+
+      if( document && document?.isConnected ) {
+        let ele: Element | null = null;
+
+        switch( type ) {
+          case "class" : {
+            ele = document.getElementsByClassName( idClass )[0];
+            break;
+          }
+          case "id" : {
+            ele =  document.getElementById( idClass ) as Element;
+            break;
+          }
+          case "tag" : {
+            ele = document.getElementsByTagName( idClass )[0];
+            break;
+          }
+        }
+
+        if( ele ) {
+          clearInterval( isMountedInt );
+          resolve( ele );
+        }
+      }
+    }, 500);
+  });
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import * as globals from "../global.css";
-  import * as colors from "../colors.css";
+  import { IsDarkMode } from "$lib/detectTheme.js";
 
   let { children } = $props();
 
+  onMount(() => {
+    IsDarkMode.loadInitialStore();
+  });
 </script>
 
 {@render children()}
